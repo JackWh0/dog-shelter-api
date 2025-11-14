0c4bda33d6971defa4a2b0c7687aea74d9e140a4
Jackson Silva <jackson.silva.who@gmail.com>
2025-11-14 18:58:46 -0300

    add Main Class

---
+++ src/main/java/dev/jackson/dog_shelter_api/DogShelterApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jackson.dog_shelter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogShelterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogShelterApiApplication.class, args);
+	}
+
+}
