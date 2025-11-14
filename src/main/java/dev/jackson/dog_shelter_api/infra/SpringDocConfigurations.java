8104ee0508394355109c2a661f82fe9b54efc632
Jackson Silva <jackson.silva.who@gmail.com>
2025-11-14 18:58:05 -0300

    add SpringDocConfigurations

---
+++ src/main/java/dev/jackson/dog_shelter_api/infra/SpringDocConfigurations.java
@@ -0,0 +1,18 @@
+package dev.jackson.dog_shelter_api.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SpringDocConfigurations {
+    @Bean
+     public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Dog Shelter API")
+                .description("You can make CRUD actions with this API"));
+    }
+}
