3b04a2439f6e1213eed3996073c1af33cad2e3f4
Jackson Silva <jackson.silva.who@gmail.com>
2025-11-14 18:57:10 -0300

    add Dog Service

---
+++ src/main/java/dev/jackson/dog_shelter_api/service/DogService.java
@@ -0,0 +1,115 @@
+package dev.jackson.dog_shelter_api.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import dev.jackson.dog_shelter_api.exception.DogNotFoundException;
+import org.springframework.stereotype.Service;
+
+import dev.jackson.dog_shelter_api.dto.DogDTO;
+import dev.jackson.dog_shelter_api.entity.Dog;
+import dev.jackson.dog_shelter_api.exception.InvalidDataException;
+import dev.jackson.dog_shelter_api.mapper.DogMapper;
+import dev.jackson.dog_shelter_api.repository.DogRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class DogService {
+
+    private final DogRepository dogRepository;
+    private final DogMapper dogMapper;
+
+    public DogService(DogRepository dogRepository, DogMapper dogMapper){
+        this.dogRepository = dogRepository;
+        this.dogMapper = dogMapper;
+    }
+
+    @Transactional
+    public DogDTO registerDog(DogDTO dogDTO){
+        verifyIfIsAlreadyRegistred(dogDTO);
+//        verifyDogSize(dogDTO.size());
+        verifyDogAge(dogDTO.age());
+        verifyDogGender(dogDTO.gender());
+
+        Dog dog = dogMapper.toModel(dogDTO);
+        dog = setDataToUpperCase(dog);
+        Dog registredDog = dogRepository.save(dog);
+        log.info("Registred dog: {}", registredDog.getName());
+
+        return dogMapper.toDTO(registredDog);
+    }
+
+    @Transactional
+    public List<DogDTO> listAll(){
+        return dogRepository.findAll().stream().map(dogMapper::toDTO).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public DogDTO findByName(String name) {
+        Dog foundBeer = dogRepository.findByName(name.toUpperCase())
+                .orElseThrow(() -> new DogNotFoundException("Dog named " + name + " was not found in the system"));
+        return dogMapper.toDTO(foundBeer);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        findDogRecord(id);
+        log.info("Deleting Dog's record: id={}", id);
+        dogRepository.deleteById(id);
+    }
+
+    public void updateDogsRecord(Long id, DogDTO dogDTO) {
+        Dog dogFoundById = findDogRecord(id);
+        log.info("Updating Dog's record: id={}", id);
+        verifyDogAge(dogDTO.age());
+        verifyDogGender(dogDTO.gender());
+
+        Dog dog = dogMapper.toModel(dogDTO);
+        dog = setDataToUpperCase(dog);
+        dogFoundById = dog;
+        dogFoundById.setId(id);
+        dogRepository.save(dogFoundById);
+    }
+
+    private void verifyIfIsAlreadyRegistred(DogDTO dogDTO){
+        String dogName = dogDTO.name();
+        Optional<Dog> optDog = dogRepository.findByName(dogName);
+        if(optDog.isPresent()){
+            String message = String.format("%s has already been registred", optDog.get().getName());
+            throw new InvalidDataException(message);
+        }
+    }
+
+
+
+    private void verifyDogAge(String dogAge) {
+        List<String> allowedAges = List.of("puppy", "adult", "elderly");
+        boolean isAAlloweAgeOption = allowedAges.stream().anyMatch(item -> item.equals(dogAge.toLowerCase()));
+        if(!isAAlloweAgeOption){
+            throw new InvalidDataException("Incorrect age option. It must be: puppy, adult or elderly");
+        }
+    }
+
+    private void verifyDogGender(String dogGender) {
+        List<String> allowedGenders = List.of("male", "female");
+        boolean isAAllowedGenderOption = allowedGenders.stream().anyMatch(item -> item.equals(dogGender.toLowerCase()));
+        if(!isAAllowedGenderOption){
+            throw new InvalidDataException("Incorrect gender option. It must be: male or female");
+        }
+    }
+
+    public Dog setDataToUpperCase(Dog dog) {
+        dog.setName(dog.getName().toUpperCase());
+        dog.setGender(dog.getGender().toUpperCase());
+        dog.setAge(dog.getAge().toUpperCase());
+
+        return dog;
+    }
+
+    public Dog findDogRecord(Long id){
+            return dogRepository.findById(id).orElseThrow(() -> new DogNotFoundException("The dog's record was not found."));
+    }
+}
