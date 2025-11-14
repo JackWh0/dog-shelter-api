8e116aa2b6937cb703cefa36125a491c5a922956
Jackson Silva <jackson.silva.who@gmail.com>
2025-11-14 18:56:07 -0300

    add Dog Entity

---
+++ src/main/java/dev/jackson/dog_shelter_api/entity/Dog.java
@@ -0,0 +1,42 @@
+package dev.jackson.dog_shelter_api.entity;
+
+import dev.jackson.dog_shelter_api.enums.DogSize;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DogSize size;
+
+}
